699be514d0d5e9ec99cb21fd547254b90a83ce2b
Choolerk <guidelmn@gmail.com>
2021-04-27 00:49:35 +0700

    --

--- backend/src/register/register.service.ts
+++ backend/src/register/register.service.ts
@@ -1,9 +1,9 @@
-import { BadRequestException, HttpStatus } from '@nestjs/common';
-import { HttpException } from '@nestjs/common';
-import { Injectable } from '@nestjs/common';
-import e from 'express';
-import { exception } from 'node:console';
-import { v4 as uuidv4 } from 'uuid';
+import { BadRequestException, HttpStatus } from "@nestjs/common";
+import { HttpException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
+import e from "express";
+import { exception } from "node:console";
+import { v4 as uuidv4 } from "uuid";
 @Injectable()
 export class RegisterService {
   register(
@@ -20,28 +20,14 @@ export class RegisterService {
     city: String,
     postCode: String,
     password: String,
-    confrimPassword: String,
+    confrimPassword: String
   ) {
     var uid = uuidv4();
-    if (
-      !firstName ||
-      !lastName ||
-      !email ||
-      !nationality ||
-      (!citizenID && !passportNumber) ||
-      !telephone ||
-      !BirthDate ||
-      !gender ||
-      !address ||
-      !city ||
-      !postCode ||
-      !password ||
-      !confrimPassword
-    ) {
+    if (!firstName) {
       throw new BadRequestException();
     } else {
       console.log(
-        ` id : ${uid} \n first : ${firstName}\n last : ${lastName}\n citizen : ${citizenID}\n passport : ${passportNumber}\n phone : ${telephone}\n BirthDate : ${BirthDate} \n gender : ${gender}\n address : ${address}\n city : ${city} \n postCode : ${postCode}\n password : ${password}\n conPass : ${confrimPassword}`,
+        ` id : ${uid} \n first : ${firstName}\n last : ${lastName}\n citizen : ${citizenID}\n passport : ${passportNumber}\n phone : ${telephone}\n BirthDate : ${BirthDate} \n gender : ${gender}\n address : ${address}\n city : ${city} \n postCode : ${postCode}\n password : ${password}\n conPass : ${confrimPassword}`
       );
     }
   }
