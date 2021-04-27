1eb19a3c0584f5b0e891a0414e782f128389d0bb
JommyXD <jom1820@gmail.com>
2021-04-27 16:19:33 +0700

    Conect to database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
 import { RegisterModule } from './register/register.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { rootCertificates } from 'node:tls';
+ 
 @Module({
-  imports: [LoginModule, RegisterModule],
+  imports: [LoginModule, RegisterModule,TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
